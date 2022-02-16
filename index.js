20e817d4437a3b4503f4df5567850422a1cdf415
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-02-16 09:21:37 -0300

    Conexao ao Banco

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const app =  require('./controllers/servicoEstoque')
+const conexaoBD = require('./infra/conexao')
 
-app.listen(4000, () => console.log('servidor rodando na porta 4000'))
+conexaoBD.connect((err) => {
+    if(err){
+        console.log(err) 
+    } else {
+
+        console.log('BD conectado com sucesso')
+
+        app.listen(4000, () => console.log("Rodando servidor na porta 4000"))
+        
+    }
+})
 
 module.exports = app
