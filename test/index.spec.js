3dcebef76d052f50d6e538878053c2400da30cba
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-02-16 10:55:59 -0300

    POST Caminho feliz

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,15 +2,26 @@ const request = require('supertest')
 const app = require('../controllers/servicoEstoque')
 
 
-describe("app Test", () => {
+describe("app Test rotas", () => {
     it('GET resultados', async () => {
         const res = await request(app).get('/estoque')
         expect(res.body).toHaveProperty('resultados')
     })
     it('GET com SBN', async () => {
         const res = await request(app).get('/estoque/0')
-        console.log(res.body.sbn)
         expect(res.body.sbn).toBe(0)
+    })
+    it('POST', async () => {
+        const sbn = 999 
+        const res = await request(app).post('/estoque')
+        .send({
+            "nome": "Testes X",
+            "sbn": `${sbn}`,
+            "autor": "Criador do teste x",
+            "estoque": "99",
+            "descricao": "livro sobre testes x"
+        })
+        expect(res.body.sbn).toBe(`${sbn}`)
     })
         
 })
