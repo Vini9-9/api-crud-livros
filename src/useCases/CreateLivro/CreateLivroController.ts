d61d30a57d34e331e39b5bd31a1dd4cfb57750dd
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-02-24 18:08:27 -0300

    Mensagens de erro Create Livro

--- src/useCases/CreateLivro/CreateLivroController.ts
+++ src/useCases/CreateLivro/CreateLivroController.ts
@@ -12,9 +12,12 @@ export class CreateLivroController {
         const {isbn, nome, autor, descricao, estoque} = request.body
 
         const result = await this.createLivroUseCase.execute({isbn, nome, autor, descricao, estoque}) 
+        const temErroCampo = typeof result === 'object'
 
         if(result instanceof Error){
             return response.status(400).json(result.message)
+        } else if(temErroCampo){
+            return response.status(400).json(result)
         }
         
         return response.status(201).send()
--- src/useCases/CreateLivro/CreateLivroUseCase.ts
+++ src/useCases/CreateLivro/CreateLivroUseCase.ts
@@ -14,7 +14,7 @@ export class CreateLivroUseCase {
         
     }
 
-    async execute({isbn, nome, autor, descricao, estoque}: IRequest): Promise<void | Error>{
+    async execute({isbn, nome, autor, descricao, estoque}: IRequest): Promise<void | Error | object[]>{
 
         const isbnJaAssociado = await this.livrosRepository.buscaPorIsbn(isbn);
 
@@ -22,6 +22,42 @@ export class CreateLivroUseCase {
             return new Error("ISBN já está associado a outro livro");
         }
 
-        this.livrosRepository.adiciona({isbn, nome, autor, descricao, estoque });
+        const sbnEhValido = isbn  
+        const nomeEhValido = nome  
+        const autorEhValido = autor 
+        const estoqueEhValido = estoque > 0 ? true : undefined;
+
+        const validacoes = [
+            {
+                campo: 'isbn',
+                valido: sbnEhValido,
+                mensagem: 'isbn deve ser preenchido'
+            },
+            {
+                campo: 'nome',
+                valido: nomeEhValido,
+                mensagem: 'Nome deve ser preenchido'
+            },
+            {
+                campo: 'autor',
+                valido: autorEhValido,
+                mensagem: 'Autor deve ser preenchido'
+            },
+            {
+                campo: 'estoque',
+                valido: estoqueEhValido,
+                mensagem: 'Estoque deve ser maior que 0'
+            }
+        ]
+        
+        const erros: Object[] = validacoes.filter( campo => !campo.valido)
+
+        const existeErro = erros.length
+
+        if(existeErro){
+            return erros
+        } else {
+            this.livrosRepository.adiciona({isbn, nome, autor, descricao, estoque });
+        }
     }
 }
