1de24261f63c656e316528f210a3b0a0c2220cb1
Vinicius Pessoa da Silva <vipdsilva@ibm.com>
2022-03-05 01:08:11 -0300

    Testes unitários ListLivroByISBN

---
+++ src/useCases/ListLivroByIsbn/ListLivroByIsbnUseCase.spec.ts
@@ -0,0 +1,71 @@
+import { Livro } from "src/entities/Livro";
+import { LivrosRepositoryInMemory } from "../../repositories/in-memory/LivrosRepositoryInMemory"
+import { CreateLivroUseCase } from "../CreateLivro/CreateLivroUseCase";
+import { ListLivroByIsbnUseCase } from "./ListLivroByIsbnUseCase"
+
+
+let livrosRepositoryInMemory: LivrosRepositoryInMemory;
+let createLivroUseCase: CreateLivroUseCase;
+let listLivroByIsbnUseCase: ListLivroByIsbnUseCase;
+let novoLivro: Livro;
+let outroLivro: Livro;
+
+describe("List Livro by ISBN", () => {
+
+    beforeEach(async () => {
+        livrosRepositoryInMemory = new LivrosRepositoryInMemory()
+        createLivroUseCase = new CreateLivroUseCase(livrosRepositoryInMemory)
+        listLivroByIsbnUseCase = new ListLivroByIsbnUseCase(livrosRepositoryInMemory)
+
+        novoLivro = {
+            isbn: "1",
+            nome: "nome do Livro",
+            autor: "autor do livro",
+            descricao: "descricao do livro",
+            estoque: 100
+        }
+
+        outroLivro = {
+            isbn: "2",
+            nome: "nome do outro Livro",
+            autor: "autor do outro livro",
+            descricao: "descricao do outro livro",
+            estoque: 102
+        }
+
+        await createLivroUseCase.execute(novoLivro)
+
+        await createLivroUseCase.execute(outroLivro)
+    })
+    
+    it("Deve localizar um livro por isbn", async () => {
+
+        const livroCriado = await listLivroByIsbnUseCase.execute(novoLivro.isbn)
+
+        expect(livroCriado).toBeDefined();
+
+        expect.objectContaining([
+            expect.objectContaining({isbn: novoLivro.isbn}),
+            expect.objectContaining({nome: novoLivro.nome}),
+            expect.objectContaining({autor: novoLivro.autor}),
+            expect.objectContaining({descricao: novoLivro.descricao}),
+            expect.objectContaining({estoque: novoLivro.estoque})
+        ])
+ 
+    })
+
+    it("Não deve listar um livro com isbn inexistente", async () => {
+
+        const isbn = "99"
+        const result = await listLivroByIsbnUseCase.execute(isbn)
+        
+        const msgErroISBN = "ISBN não localizado"
+        
+        expect(result).toBeInstanceOf(Error);
+
+        expect.arrayContaining([
+            expect.objectContaining({Error: msgErroISBN}),
+          ])
+ 
+    })
+})
